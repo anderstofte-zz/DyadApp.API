e59ff64c573c83236976d8cac22f75a5debe5a09
Anders <andersrta@gmail.com>
2020-04-21 12:32:57 +0200

    Add authentication and authorization.

--- DyadApp.API/Controllers/UsersController.cs
+++ DyadApp.API/Controllers/UsersController.cs
@@ -1,24 +1,36 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DyadApp.API.Converters;
 using DyadApp.API.Data;
+using DyadApp.API.Services;
 using DyadApp.API.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DyadApp.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DyadAppContext _context;
-
-        public UsersController(DyadAppContext context)
+        private readonly IAuthenticationService _auth;
+        public UsersController(DyadAppContext context, IAuthenticationService auth)
         {
             _context = context;
+            _auth = auth;
         }
 
+        public string TestAuthorization()
+        {
+            return "Authorized!";
+        }
+
+        
         [HttpPost]
+        [AllowAnonymous]
         public async Task<ActionResult> CreateUser([FromBody] CreateUserModel model)
         {
             if (!ModelState.IsValid)
@@ -26,8 +38,12 @@ namespace DyadApp.API.Controllers
                 return BadRequest();
             }
 
-            var currentDateTime = DateTime.Now;
+            if (UserWithProvidedEmailAlreadyExists(model.Email))
+            {
+                return BadRequest("The email already exists.");
+            }
 
+            var currentDateTime = DateTime.Now;
             var user = model.ToUser();
             user.Modified = currentDateTime;
             user.ModifiedBy = 0;
@@ -37,7 +53,14 @@ namespace DyadApp.API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return Ok();
+            var token = await _auth.Authenticate(model.Email, model.Password);
+
+            return Ok(token);
+        }
+
+        private bool UserWithProvidedEmailAlreadyExists(string email)
+        {
+            return _context.Users.Any(x => x.Email == email);
         }
     }
 }
---
+++ DyadApp.API/Services/AuthenticationService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DyadApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DyadApp.API.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly DyadAppContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationService(DyadAppContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<string> Authenticate(string email, string password)
+        {
+            var user = await _context.Users.SingleAsync(x => x.Email == email && x.Password == password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(System.IO.File.ReadAllText("key.txt"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserId.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
