a3ddbdb13de7c0d6154ee4ba77da79a364db1254
Anders <ande587e.stud.kea.dk>
2020-05-26 15:48:46 +0200

    Logging

--- DyadApp.API/Controllers/AccountController.cs
+++ DyadApp.API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using DyadApp.API.Data.Repositories;
 using DyadApp.API.Extensions;
+using DyadApp.API.Models;
+using DyadApp.API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +14,25 @@ namespace DyadApp.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
-
-        public AccountController(IUserRepository userRepository)
+        private readonly ILoggingService _loggingService;
+        public AccountController(IUserRepository userRepository, ILoggingService loggingService)
         {
             _userRepository = userRepository;
+            _loggingService = loggingService;
         }
 
         [HttpPost("Status")]
         public async Task<IActionResult> SetAccountStatus([FromBody] bool status)
         {
+            var message = status
+                ? $"Activated account with user id {User.GetUserId()}."
+                : $"Deactivated account with user id {User.GetUserId()}.";
+            var action = status
+                ? AuditActionEnum.Activate
+                : AuditActionEnum.Deactivate;
+
+            await _loggingService.SaveAuditLog(message, action);
+
             var user = await _userRepository.GetUserById(User.GetUserId());
             user.Active = status;
             await _userRepository.SaveChangesAsync();
---
+++ DyadApp.API/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using DyadApp.API.Extensions;
+using DyadApp.API.Models;
+using DyadApp.API.Services;
+using DyadApp.API.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DyadApp.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IChatService _chatService;
+        private readonly ILoggingService _loggingService;
+        public ChatController(IChatService chatService, ILoggingService loggingService)
+        {
+            _chatService = chatService;
+            _loggingService = loggingService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<MatchConversationModel> FetchChat(int id)
+        {
+            var userId = User.GetUserId();
+            await _loggingService.SaveAuditLog($"Retrieving match with id {id} for user with user id {userId}",
+                AuditActionEnum.Read);
+            return await _chatService.FetchChatMessages(id, userId);
+        }
+
+        [HttpPost("Read/{id}")]
+        public async Task MessagesIsRead(int id)
+        {
+            var userId = User.GetUserId();
+            await _loggingService.SaveAuditLog($"Updating chat messages for match with id {id} and user id {userId}",
+                AuditActionEnum.Update);
+            await _chatService.MarkMessagesAsRead(id, userId);
+        }
+    }
+}
